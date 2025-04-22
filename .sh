67bf72bc2881cacfbb57a1d6bca9010cefedbdcf
Web4 <167559384+Web4application@users.noreply.github.com>
2025-04-21 18:02:55 -0700

    Update .sh

--- .sh
+++ .sh
@@ -2,3 +2,4 @@ npm install -g vercel
 https://github.com/Web4application/script_analyzer_bot.git
 python3 -m venv venv
 source venv/bin/activate  # Use venv\Scripts\activate on Windows
+pip install -r requirements.txt
