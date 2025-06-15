4063c21012e32f6cfc14e996d7501b144afe4d74
Web4 <167559384+Web4application@users.noreply.github.com>
2025-06-16 00:57:47 +0200

    Update Bash.sh

--- Bash.sh
+++ Bash.sh
@@ -4,3 +4,8 @@ python3 -m venv venv
 source venv/bin/activate  # Use venv\Scripts\activate on Windows
 pip install -r requirements.txt
 
+cd backend
+pip install fastapi uvicorn
+touch main.py
+
+npx create-next-app@latest [lola-ai] [https://nextjs-boilerplate-web4application.vercel.app]
